37a69c0160d36bf6f628317be26c9370e08b9914
nirokay <80123634+nirokay@users.noreply.github.com>
2023-10-25 11:36:02 +0200

    corrected projectName variable in package.nim

--- package.nim
+++ package.nim
@@ -1,7 +1,11 @@
+# import pkginfo
+
+# let pkg = pkg("docchanger")
+
 const
-    projectName*: string = "topicdocchanger"
-    projectDescription*: string = ""
-    projectVersion*: string = "v" & "0.1.0"
-    projectAuthors*: seq[string] = @["nirokay"]
-    projectLicence*: string = "GPL-3.0"
-    projectSource*: string = "https://github.com/nirokay/topicdocchanger"
+    projectName*: string = "docchanger" # pkg.getName()
+    projectDescription*: string = "Replaces specific xml text in .docx files." # pkg.getDescription()
+    projectVersion*: string = "v0.1.0" # "v" & pkg.version()
+    projectAuthors*: seq[string] = @["nirokay"] # @[pkg.getAuthor()]
+    projectLicence*: string = "GPL-3.0-only" # pkg.getLicense()
+    projectSource*: string = "https://github.com/nirokay/DocChanger/"
